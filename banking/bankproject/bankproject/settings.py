dc279e360ba71b6f53d79ccee34204f4a86d270f
bkathul <154122528+bkathul@users.noreply.github.com>
2024-02-06 15:34:36 +0530

    Update settings.py

--- banking/bankproject/bankproject/settings.py
+++ banking/bankproject/bankproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0w9nqxjgk136fbyakt77==r4h4oc7fqryogj7cu(p%&isp==%z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
